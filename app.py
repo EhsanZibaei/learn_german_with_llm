264fbd3a0ff3b4e07a46e26e9e9166ea4668e31f
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2024-08-18 22:53:43 +0200

    add openai audio to text function

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template_string, render_template
 from flask import request
 import os
-from openai import OpenAI
 from env.openai_key import API_KEY
+import openai
+from openai import OpenAI
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ os.environ["OPENAI_API_KEY"] = API_KEY
 OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 MODEL_NAME = "gpt-4o"
 SEED = 42
-client = OpenAI()
+client = openai.OpenAI()
 
 
 # send chatgpt request
@@ -49,10 +50,24 @@ def main_index():
         print("wow")
         file = request.files['audio']
         file.save("uploads/my_sound.wav")
+        user_input=audio_to_text()
+
+
     if request.method=='GET':
         user_input = "GET1"
         gpt_output = "GET2"
     return render_template('index.html', user_input=user_input, gpt_output = gpt_output)
+
+def audio_to_text():
+    audio_file = open("uploads/my_sound.wav", "rb")
+    transcription = openai.Audio.transcribe(
+        model="whisper-1",
+        file=audio_file,
+        response_format="text",
+        language="de"
+    )
+    print("---------ehsan says: " + transcription['text'])
+    return transcription['text']
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
